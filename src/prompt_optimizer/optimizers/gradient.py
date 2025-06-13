e0645249d988c497a338be82100d55e748fa11a0
Taylor Agarwal <taagarwa@redhat.com>
2025-06-13 09:16:15 -0400

    :bug: Update base gradient prompt to correct prompt

--- src/prompt_optimizer/optimizers/gradient.py
+++ src/prompt_optimizer/optimizers/gradient.py
@@ -18,7 +18,8 @@ But this prompt gets the following examples wrong:
 {error_string}
 give {num_feedbacks} reasons why the prompt could
 have gotten these examples wrong.
-Respond using JSON only."""
+Wrap each reason with <START> and <END>"""
+
 
 REWRITE_PROMPT = """I'm trying to write a zero-shot classifier prompt.
 My current prompt is:
@@ -26,8 +27,10 @@ My current prompt is:
 But it gets the following examples wrong:
 {error_string}
 Based on these examples the problem with this prompt is that {gradient}
-Based on the above information, write {steps_per_gradient} different improved prompts.
-Respond using JSON only."""
+Based on the above information, I wrote {steps_per_gradient} different improved prompts.
+Each prompt is wrapped with <START> and <END>.
+The {steps_per_gradient} new prompts are:
+"""
 
 PipelineOutputType = Any
 
